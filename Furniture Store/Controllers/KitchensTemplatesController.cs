a4485c80732b03c4adf78e00f3c93207517eba9f
Maruna Butenko <marina21nsd@gmail.com>
2023-06-24 16:15:32 +0300

    save changes

--- Furniture Store/Controllers/KitchensTemplatesController.cs
+++ Furniture Store/Controllers/KitchensTemplatesController.cs
@@ -1,6 +1,7 @@
 ﻿using Furniture_Store.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Storage;
+using System.IO;
 
 namespace Furniture_Store.Controllers
 {
@@ -15,29 +16,32 @@ namespace Furniture_Store.Controllers
             var kTamplates = new List<KitchensTemplate>();
 
             var tamplate1 = new KitchensTemplate();
-            //tamplate1.Type = 'a';
-            //tamplate1.Description = "direct";
+            tamplate1.KitchenType = "a";
+            tamplate1.KitchenDescription = "direct";
             kTamplates.Add(tamplate1);
 
             var tamplate2 = new KitchensTemplate();
-            //tamplate2.Type = 'b';
-            //tamplate2.Description = "cornerLeft";
+            tamplate2.KitchenType = "b";
+            tamplate2.KitchenDescription = "cornerLeft";
             kTamplates.Add(tamplate2);
 
             var tamplate3 = new KitchensTemplate();
-            //tamplate3.Type = 'c';
-            //tamplate3.Description = "cornerRight";
+            tamplate3.KitchenType = "c";
+            tamplate3.KitchenDescription = "cornerRight";
             kTamplates.Add(tamplate3);
 
             var tamplate4 = new KitchensTemplate();
-            //tamplate4.Type = 'd';
-            //tamplate4.Description = "pSimilar";
+            tamplate4.KitchenType = "d";
+            tamplate4.KitchenDescription = "pSimilar";
             kTamplates.Add(tamplate4);
 
             return kTamplates;
+           //return Content($"available kitchen configurations: {tamplate1.KitchenDescription}, {tamplate2.KitchenDescription}, " +
+           //    $"{tamplate3.KitchenDescription}");
+
         }
 
 
-       
+
     }
 }
